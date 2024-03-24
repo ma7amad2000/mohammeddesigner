640cc39c311365b9f12a76b5a5adac689975a489
مشاريع حياه <126801003+ma7amad2000@users.noreply.github.com>
2024-03-24 14:26:23 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
-document.getElementById("contact-form").addEventListener("submit", function (event) {
-    event.preventDefault(); // Prevent default form submission
-    let name = document.getElementById("name").value;
-    let email = document.getElementById("email").value;
-    let message = document.getElementById("message").value;
+// document.getElementById("contact-form").addEventListener("submit", function (event) {
+//     event.preventDefault(); // Prevent default form submission
+//     let name = document.getElementById("Name").value;
+//     let email = document.getElementById("Email").value;
+//     let subject = document.getElementById("subject").value;
+//     let message = document.getElementById("Message").value;
 
-    // You can replace 'your-email@example.com' with your email address
-    window.location.href = "sara.mktaz@gmail.com?subject=Message from " + name + " (" + email + ")&body=" + message;
-});
+//     // You can replace 'your-email@example.com' with your email address
+//     window.location.href = "sara.mktaz@gmail.com?subject=Message from " + name + " (" + email + ")&body=" + subject + message;
+// });
 
 
 const menu = document.getElementById("menu");
